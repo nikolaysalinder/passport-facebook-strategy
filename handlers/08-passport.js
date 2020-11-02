707125f71218f53d607b7144cb70b89c782e3c0e
Nikolay Salinder <nikolay.salinder@gmail.com>
2020-11-02 14:13:17 +0500

    Add some desc

--- handlers/08-passport.js
+++ handlers/08-passport.js
@@ -1,6 +1,10 @@
 const passport = require('koa-passport')
 
 exports.init = app => {
+  // passport initialize добавляет 3 метода
+  // 1) isAuthenticated()
+  // 2) login()
+  // 3) logout()
   app.use(passport.initialize());
   app.use(passport.session()); // ctx.state.user = user
 };
